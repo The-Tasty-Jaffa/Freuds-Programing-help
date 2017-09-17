9f380969f5dc78f1856c47b132b4164905638947
The-Tasty-Jaffa <The-Tasty-Jaffa@users.noreply.github.com>
2017-09-17 21:15:09 +0100

    Update hunt.py

--- hunt/hunt.py
+++ hunt/hunt.py
@@ -12,34 +12,26 @@ class Hunt:
         self.answers = dataIO.load_json("data/Tasty/Hunt/answers.json")
         self.riddles = dataIO.load_json("data/Tasty/Hunt/riddles.json")
         self.settings = dataIO.load_json("data/Tasty/Hunt/settings.json")
-        #self.set_up_commands()
-
-    #def Default_response_Command(self, ctx):
-     #   msg = ctx.message.content
-	#if msg[1:-1] != self.answers[:-1]:
-	 #   await self.bot.send_message(ctx.message.channel, self.riddles[self.answers.index(msg)])
-#        elif msg == self.answers[-1]: #If they Get the final Command will add "Puzzle King/Queen" role
- #           await self.bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, name="Puzzle King/Queen"))
-  #          await self.bot.send_message(ctx.message.channel, self.riddles[-1])
             
-   # def set_up_commands(self):
-	
-#        for x in self.answers:
- #           self.bot.add_command(name=x, callback=Default_response_Command, pass_context=True, hidden=True)
     
     @commands.command(pass_context=True, no_pm=True)
     async def hunt(self, ctx):
-        pm = await self.bot.start_private_message(ctx.message.author)
-        await self.bot.send_message(pm, "Welcome {} to the treasure hunt! Can you find the final hidden command and claim your reward? \nEach riddle will get harder and harder so good luck!\n**Write the answer to the riddles as a command in any channel (-[answer])**".format(ctx.message.author.mention))
         server = ctx.message.server
         member = ctx.message.author
-        role = discord.utils.get(server.roles, name="Puzzle Hunter")
-        await self.bot.add_roles(member, role)
         score = 0
+        role = discord.utils.get(server.roles, name="Puzzle Hunter")
+
+        await self.bot.send_message(ctx.message.author, "Welcome {} to the treasure hunt! Can you find the final hidden command and claim your reward? \nEach riddle will get harder and harder so good luck!\n**Write the answer to the riddles as a command in any channel (-[answer])**".format(ctx.message.author.mention))
+        await self.bot.add_roles(member, role)
+        
         for index, question in enumerate(self.riddles):
-            await self.bot.send_message(pm, question)
+            await self.bot.send_message(member, question)
             msg = await self.bot.wait_for_message(author=ctx.message.author)
-            await self.bot.delete_message(msg)
+            if isinstance(msg.channel.type, type(discord.ChannelType.private)):
+                pass
+            else:
+                await self.bot.delete_message(msg)
+
             if msg.content[1:-1] == self.answers[index]:
                 score += 1
                 await self.bot.delete_message(ctx.message)
@@ -47,6 +39,8 @@ class Hunt:
         if score >= 10:
           role = discord.utils.get(server.roles, name="Puzzle King/Queen")
           await self.bot.add_roles(member, role)
+
+
     @checks.is_owner()
     @commands.command(hidden=True, pass_context=True)
     async def sethunt(self, ctx): #This is a cheat method because the bot will only do it for one 
@@ -108,4 +102,3 @@ def setup(bot):
     check_folders()
     check_files()
     bot.add_cog(Hunt(bot))
-
